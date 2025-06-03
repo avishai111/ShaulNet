3ddfe9047bafecc6d27214bbde0b49b79405d7b4
Your Name <you@example.com>
2025-06-03 22:10:03 +0000

    reordering files

--- inference.py
+++ inference.py
@@ -1,8 +1,10 @@
 import torch
+import datetime
 import librosa
 import numpy as np
 import soundfile as sf
 from pathlib import Path
+import matplotlib.pyplot as plt
 from omegaconf import DictConfig, OmegaConf
 import hydra
 from Data_Creation_scripts.HebrewToEnglish import HebrewToEnglish
@@ -15,6 +17,7 @@ from Matcha_TTS.matcha.hifigan.models import Generator as MatchaVocoder
 from Matcha_TTS.matcha.hifigan.config import v1 as matcha_hifigan_config
 from Matcha_TTS.matcha.hifigan.denoiser import Denoiser as MatchaDenoiser
 from Matcha_TTS.matcha.text import text_to_sequence as matcha_text_to_sequence
+from Matcha_TTS.matcha.text import sequence_to_text as matcha_sequence_to_text
 from Matcha_TTS.matcha.utils.utils import intersperse
 
 # === Tacotron2 ===
@@ -22,15 +25,64 @@ from speechbrain.inference.TTS import Tacotron2 as Tacotron2
 from speechbrain.inference.vocoders import HIFIGAN as SBHifiGAN
 from tracatron2.text import text_to_sequence as tacotron_text_to_sequence
 
+# === Plotting ===
+def plot_mel(mel, title="Mel-Spectrogram"):
+    """
+    Plots a mel-spectrogram.
+
+    Args:
+        mel (np.ndarray or torch.Tensor): Mel spectrogram to plot, shape [n_mels, time].
+        title (str): Title of the plot.
+    """
+    if torch.is_tensor(mel):
+        mel = mel.detach().cpu().numpy().squeeze()
+
+    plt.figure(figsize=(10, 4))
+    plt.imshow(mel, aspect='auto', origin='lower', interpolation='none')
+    plt.title(title)
+    plt.xlabel('Time')
+    plt.ylabel('Mel Bin')
+    plt.colorbar(format="%+2.0f dB")
+    plt.tight_layout()
+    plt.show()
 
 # === MatchaTTS model inference ===
+
+@torch.inference_mode()
+def process_text(text: str, device: torch.device):
+    x = torch.tensor(intersperse(matcha_text_to_sequence(text, ['basic_cleaners'])[0], 0),dtype=torch.long, device=device)[None]
+    x_lengths = torch.tensor([x.shape[-1]],dtype=torch.long, device=device)
+    x_phones = matcha_sequence_to_text(x.squeeze(0).tolist())
+    return {
+        'x_orig': text,
+        'x': x,
+        'x_lengths': x_lengths,
+        'x_phones': x_phones
+    }
+
 @torch.inference_mode()
 def infer_matcha(model_cfg, text, device):
+    ## Number of ODE Solver steps
+    n_timesteps = 10
+    ## Changes to the speaking rate
+    length_scale = 1
+    ## Sampling temperature
+    temperature = 0.1
+    # Load the model
     model = MatchaTTS.load_from_checkpoint(model_cfg.checkpoint, map_location=device).eval().to(device)
-    seq = torch.tensor(intersperse(matcha_text_to_sequence(text, ['basic_cleaners'])[0], 0),
-                       dtype=torch.long, device=device)[None]
-    mel = model.synthesise(seq, torch.tensor([seq.shape[-1]], device=device))[0]['mel']
-    return mel
+    text_processed = process_text(text, device)
+    start_t = datetime.datetime.now()
+    output = model.synthesise(
+        text_processed['x'], 
+        text_processed['x_lengths'],
+        n_timesteps=n_timesteps,
+        temperature=temperature,
+        spks=None,
+        length_scale=length_scale
+    )
+    # merge everything to one dict    
+    output.update({'start_t': start_t, **text_processed})
+    return output
 
 # === Tacotron2 model inference ===
 @torch.inference_mode()
@@ -43,7 +95,7 @@ def infer_tacotron2(model_cfg, text, device):
 
 # === Vocoder inference ===
 @torch.inference_mode()
-def vocode_hifigan(vocoder_cfg, mel, model_type, device):
+def vocode_hifigan(vocoder_cfg, mel, model_type, device): # Until here I CHECKED!!!
     if model_type == "matcha":
         voc = MatchaVocoder(matcha_hifigan_config).to(device)
         voc.load_state_dict(torch.load(vocoder_cfg.checkpoint, map_location=device)['generator'])
